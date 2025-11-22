13121e2154367d69fa8564a39c3b0ab111fd0260
Maira <stefaniamairatorres@gmail.com>
2025-11-22 10:30:33 -0300

    Add Product model

---
+++ src/models/ProductModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
